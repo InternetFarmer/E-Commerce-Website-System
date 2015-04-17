963dd9f7680b2501c5ade703b060fa622cae6427
zyyswallow <yanyanzhou@dl-wireless-pittnet-150-212-45-76.wireless.pitt.edu>
2015-04-16 21:22:34 -0400

    service done!

---
+++ src/java/edu/pitt/sis/infsci2730/finalProject/service/RecordService.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pitt.sis.infsci2730.finalProject.service;
+
+import edu.pitt.sis.infsci2730.finalProject.bean.Record;
+import edu.pitt.sis.infsci2730.finalProject.dao.RecordDao;
+import edu.pitt.sis.infsci2730.finalProject.rowMapper.RecordRowMapper;
+import java.util.List;
+
+/**
+ *
+ * @author yanyanzhou
+ */
+public class RecordService {
+    //search record by transaction_id
+    public  List<Record> GetRecordByTransactionID(final String id) {
+        return RecordDao.GetRecordByProductID(id);
+    }
+
+    //search record by product_id
+    public  List<Record> GetRecordByProductID(final String id) {
+        return RecordDao.GetRecordByProductID(id);
+    }
+
+    //insert new record by transaction_id
+    public  int InsertRecordByTransactionIDAndProductId(final String[] array) {
+        return RecordDao.InsertRecordByTransactionIDAndProductId(array);
+    }
+
+    //delete records by transaction_id
+    public  int DeleteRecordByTransactionID(final String id) {
+        return RecordDao.DeleteRecordByTransactionID(id);
+    }
+
+    //delete records by product_id
+    public  int DeleteRecordByProductID(final String id) {
+        return RecordDao.DeleteRecordByProductID(id);
+    }
+
+    //update records by transaction_id and product_id
+    public  int UpdateRecordByTransactionIDAndProductID(final String[] array) {
+        return RecordDao.UpdateRecordByTransactionIDAndProductID(array);
+                }
+}
---
+++ src/java/edu/pitt/sis/infsci2730/finalProject/service/TransactionService.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pitt.sis.infsci2730.finalProject.service;
+
+import edu.pitt.sis.infsci2730.finalProject.bean.Transaction;
+import edu.pitt.sis.infsci2730.finalProject.dao.TransactionDao;
+import edu.pitt.sis.infsci2730.finalProject.rowMapper.TransactionRowMapper;
+import java.util.List;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+/**
+ *
+ * @author yanyanzhou
+ */
+public class TransactionService {
+
+    //search transactions and records by transaction_id
+    public Transaction GetTransactionByID(final String id) {
+        return TransactionDao.GetTransactionByID(id);
+    }
+
+    //search transactions by customer_id
+    public List<Transaction> GetTransactionByCustomerID(final String id) {
+        return TransactionDao.GetTransactionByCustomerID(id);
+    }
+
+    //search transactions and records by customer_id
+    public  SqlRowSet GetTransaction(final String id) {
+        return TransactionDao.GetTransaction(id);
+    }
+    
+    public List<Transaction> GetAllTransaction() {
+        return TransactionDao.GetAllTransaction();
+    }
+    
+    public static SqlRowSet GetTranactionTotalAmount(final String id) {
+        return TransactionDao.GetTranactionTotalAmount(id);
+    }
+
+    //search transactions by date (a period of time)
+    public static SqlRowSet GetTransactionByDate(final String[] array) {
+        return TransactionDao.GetTransactionByDate(array);
+    }
+
+    //insert new transaction by Transaction_id
+    public static int InsertTransactionByID(final String[] array) {
+        return TransactionDao.InsertTransactionByID(array);
+    }
+
+    //delete transactions by id
+    public static int DeleteTransactionByID(final String id) {
+        return TransactionDao.DeleteTransactionByID(id);
+    }
+
+    //update transactions by id
+    public static int UpdateTransactionByID(final String[] array) {
+        return TransactionDao.UpdateTransactionByID(array);
+    }
+}
