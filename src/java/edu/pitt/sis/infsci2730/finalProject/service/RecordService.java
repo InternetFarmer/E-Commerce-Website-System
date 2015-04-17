68dfbfcc1db8deff94915db5917f14be77301962
Jiadong Wu <wuplus1992@gmail.com>
2015-04-16 21:58:08 -0400

    update

--- src/java/edu/pitt/sis/infsci2730/finalProject/service/RecordService.java
+++ src/java/edu/pitt/sis/infsci2730/finalProject/service/RecordService.java
@@ -14,33 +14,35 @@ import java.util.List;
  * @author yanyanzhou
  */
 public class RecordService {
+
     //search record by transaction_id
-    public  List<Record> GetRecordByTransactionID(final String id) {
+
+    public List<Record> GetRecordByTransactionID(final String id) {
         return RecordDao.GetRecordByProductID(id);
     }
 
     //search record by product_id
-    public  List<Record> GetRecordByProductID(final String id) {
+    public List<Record> GetRecordByProductID(final String id) {
         return RecordDao.GetRecordByProductID(id);
     }
 
     //insert new record by transaction_id
-    public  int InsertRecordByTransactionIDAndProductId(final String[] array) {
+    public int InsertRecordByTransactionIDAndProductId(final String[] array) {
         return RecordDao.InsertRecordByTransactionIDAndProductId(array);
     }
 
     //delete records by transaction_id
-    public  int DeleteRecordByTransactionID(final String id) {
+    public int DeleteRecordByTransactionID(final String id) {
         return RecordDao.DeleteRecordByTransactionID(id);
     }
 
     //delete records by product_id
-    public  int DeleteRecordByProductID(final String id) {
+    public int DeleteRecordByProductID(final String id) {
         return RecordDao.DeleteRecordByProductID(id);
     }
 
     //update records by transaction_id and product_id
-    public  int UpdateRecordByTransactionIDAndProductID(final String[] array) {
+    public int UpdateRecordByTransactionIDAndProductID(final String[] array) {
         return RecordDao.UpdateRecordByTransactionIDAndProductID(array);
-                }
+    }
 }
