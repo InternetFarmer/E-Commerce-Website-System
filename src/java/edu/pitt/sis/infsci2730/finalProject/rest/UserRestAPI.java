7ce385bd5d288c22d3d2402f166390d0c4ca1068
Jiadong Wu <wuplus1992@gmail.com>
2015-04-17 04:28:17 -0400

    update

---
+++ src/java/edu/pitt/sis/infsci2730/finalProject/rest/UserRestAPI.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pitt.sis.infsci2730.finalProject.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Wu
+ */
+@Controller
+@RequestMapping("/rest/products")
+public class UserRestAPI {
+    //以下是RESTful API
+    //显示所有商品 GET
+    
+    //查询特定id 的商品 /{product_id} GET
+    
+    //增加商品 输入json return json PUT
+    
+    //修改商品 输入json return json POST
+    
+    //删除特定id的商品 /{product_id} DELETE
+}
--- src/java/edu/pitt/sis/infsci2730/finalProject/web/ProductsController.java
+++ src/java/edu/pitt/sis/infsci2730/finalProject/web/ProductsController.java
@@ -5,10 +5,49 @@
  */
 package edu.pitt.sis.infsci2730.finalProject.web;
 
+import edu.pitt.sis.infsci2730.finalProject.service.ProductService;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
 /**
  *
  * @author Wu
  */
+@Controller
+@RequestMapping("/products")
 public class ProductsController {
+    
+    private ProductService productService = new ProductService();
+    
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            Map<String, Object> myModel = new HashMap<String, Object>();
+            myModel.put("products", this.productService.GetAllProduct());
+            
+            return new ModelAndView("products", "model", myModel);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductsController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ModelAndView("500");
+        }
+    }
+
+    public void setProductManager(ProductService productService) {
+        this.productService = productService;
+    }
+    
+    
+    
     
 }
