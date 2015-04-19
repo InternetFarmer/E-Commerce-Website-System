3c959368f09ff2d73310cb26e014e04c058aaa1e
Jiadong Wu <wuplus1992@gmail.com>
2015-04-18 21:25:48 -0400

    update

--- web/resources/js/history.js
+++ web/resources/js/history.js
@@ -7,8 +7,12 @@ $(function() {
             dataType: "json",
             contentType: "application/json",
             success: function(records) {
-                $.each(records,function(record){
+                $.each(records,function(index,record){
 					console.log(record);
+					$("#recordDisplay").append("<tr><td>" + record.product_id + "</td>\n\
+                                <td>" + record.product.product_name + "</td>\n\
+                                <td>" + record.amount + "</td>\n\
+                                <td>" + record.price + "</td></tr>")
                 });
             },
             error: function() {
